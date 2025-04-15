cbef5fce5f10d968095a493029f0eb3aac2af6fb
Nguyễn Trung <trungnguyenthanh.2304@gmail.com>
2025-04-15 13:21:35 +0700

    baudrate_generator

--- hdl/baudrate_generator.sv
+++ hdl/baudrate_generator.sv
@@ -7,5 +7,41 @@ module baudrate_generator#(
     output tx_tick,
     output rx_tick,
 );
-    
+    localparam TX_COUNT_TO = CFG_CLK_FREQ / (CFG_BAUDRATE * 16);
+    localparam RX_COUNT_TO = CFG_CLK_FREQ / (CFG_BAUDRATE * 16);
+    localparam TX_COUNT_WIDTH = $clog2(TX_COUNT_MAX);
+    localparam RX_COUNT_WIDTH = $clog2(RX_COUNT_MAX);
+
+    logic [TX_COUNT_WIDTH-1:0] tx_count;
+    logic [RX_COUNT_WIDTH-1:0] rx_count;
+
+    //Counting for TX
+    always_ff @(posedge clk or negedge rst_n) begin : proc_tx_count
+        if(~rst_n) begin
+            tx_count <= 0;
+        end else begin
+            if (tx_count != TX_COUNT_TO) begin
+                tx_count <= tx_count + 1;
+            end else begin
+                tx_count <= 0;
+            end
+        end
+    end
+
+    //Counting for RX
+    always_ff @(posedge clk or negedge rst_n) begin : proc_rx_count
+        if(~rst_n) begin
+            rx_count <= 0;
+        end else begin
+            if (rx_count != RX_COUNT_TO) begin
+                rx_count <= rx_count + 1;
+            end else begin
+                rx_count <= 0;
+            end
+        end
+    end
+
+    //Tick generation for TX and RX
+    assign tx_tick = (tx_count == TX_COUNT_TO);
+    assign rx_tick = (rx_count == RX_COUNT_TO);
 endmodule
